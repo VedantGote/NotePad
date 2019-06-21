432e650e702a19fa3883ec0fb1ded344c42413c2
VedantGote <vedantg1998@gmail.com>
2019-06-21 23:34:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-x5hdd1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-x5hdd1)
---
+++ index.js
@@ -0,0 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react"
+import ReactDom from "react-dom"
+
+import App from "./App.js"
+
+ReactDom.render(<App/>,document.getElementById("root"))
